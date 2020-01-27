34dfe7e6e49f9d4b19a3b94931337d7a6d16cc7d
unknown <dj.zaragoza@icloud.com>
2020-01-26 17:04:24 -0800

    finished index.js file

---
+++ index.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const graphqlHTTP = require('express-graphql');
+const schema = require('./schema');
+const resolvers = require('./resolvers');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', function(req, res) {
+    res.sendFile('./index.htl', { root: __dirname });
+});
+
+app.use('/api', graphqlHTTP({
+    schema: schema,
+    rootValue: resolvers,
+    graphiql: true,
+}));
+
+app.listen(8000, () => {
+    console.log('App is listening on http://localhost:8000/');
+});
