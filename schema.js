2bbea1359d3cd0b146c7f79d20c016aa562330c3
unknown <dj.zaragoza@icloud.com>
2020-01-27 07:53:48 -0800

    finished schema file

---
+++ schema.js
@@ -0,0 +1,15 @@
+const buildSchema = require('graphql').buildSchema;
+
+const schema = `
+    type Query {
+        messages: [String]
+    }
+    
+    type Mutation {
+        sendMessage(DisplayName: String!, message: String!): Boolean
+    }
+`;
+
+module.exports = buildSchema(schema);
+
+
